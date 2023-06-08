52ed802b3ede998099d53b79d354f7b4c2406d3c
Drimkore <art.2000m.ab@gmail.com>
2023-06-08 16:13:41 +0500

    fix song llist and make dj function

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template, request
+from gradio_client import Client
+import json, random
 
 
 app = Flask(__name__)
-text = ''
+
 
 @app.route("/")
 def main_page():
     return render_template('index.html')
 
+
+def get_song():
+    r_id = random.randint(0, 10)
+    song_data = json.load(open('song_list.json', 'rb'))
+    song_title = song_data['songs'][r_id]['song_name']
+    return song_title
+
+
+def get_image(input_text):
+    client = Client("https://dukujames-text-image.hf.space/")
+    result = client.predict(
+				input_text,	# str  in 'Input' Textbox component
+				api_name="/predict"
+    )
+    return result
