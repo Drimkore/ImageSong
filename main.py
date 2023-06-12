95451a7b1cb30eb2735aeed208dce5301d6cd63d
XYPMA-11 <danilkop2000@mail.ru>
2023-06-12 19:42:25 +0500

    check method change_path

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 from gradio_client import Client
-import json, random
+import json, random, os
 
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route("/")
 def main_page():
+    print('Hi', flush=True)
     return render_template('index.html')
 
 
@@ -18,11 +20,23 @@ def get_song():
     return song_title
 
 
-@app.route("/", method=['POST'])
+def change_path(path):
+    path_list = path.split('/', '\\')
+    file_name = path_list[-1]
+    if os.name == 'nt':  # for windows
+        new_path = '/static/tmp/'+file_name
+        os.system(f'copy {path} {new_path}')
+    else:
+        new_path = ''+file_name
+        os.system(f'cp {path} {new_path}')
+    return new_path
+
+@app.route("/", methods=['POST'])
 def get_image():
     client = Client("https://dukujames-text-image.hf.space/")
     result = client.predict(
         get_song(),	 # str  in 'Input' Textbox component
         api_name="/predict"
     )
-    return json.dumps({'result':result})
+    print('check')
+    return json.dumps({'result':change_path(result)})
