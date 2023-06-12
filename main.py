e4b59bb53bb5d1d4ce4a1dda3f7dbdaf1b18f920
Drimkore <art.2000m.ab@gmail.com>
2023-06-12 19:11:20 +0500

    change file path

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ def main_page():
     return render_template('index.html')
 
 
+def change_path(path):
+    path_list = path.split('/', '\\')
+    file_name = path_list[-1]
+    if os.name == 'nt':  # for windows
+        new_path = ''+file_name
+        os.system(f'copy {path} {new_path}')
+    else:
+        new_path = ''+file_name
+        os.system(f'cp {path} {new_path}')
+    return new_path
+
+
 def get_song():
     r_id = random.randint(0, 10)
     song_data = json.load(open('song_list.json', 'rb'))
@@ -25,4 +37,4 @@ def get_image():
         get_song(),	 # str  in 'Input' Textbox component
         api_name="/predict"
     )
-    return json.dumps({'result':result})
+    return json.dumps({'result':change_path(result)})
