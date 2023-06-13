54bc9b72d48d7debc600cb50f668c17da8816fca
Drimkore <art.2000m.ab@gmail.com>
2023-06-13 18:58:02 +0500

    Починил смену путей

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ def main_page():
 
 
 def change_path(path):
-    path_list = path.split('/', '\\')
-    file_name = path_list[-1]
     if os.name == 'nt':  # for windows
-        new_path = ''+file_name
+        path_list = path.split('\\')
+        file_name = path_list[-1]
+        new_path = ''+file_name  # по аналогии
         os.system(f'copy {path} {new_path}')
-    else:
-        new_path = ''+file_name
+    else:  #  for unix
+        path_list = path.split('/')
+        file_name = path_list[-1]
+        new_path = 'static/'+file_name
         os.system(f'cp {path} {new_path}')
     return new_path
+
 
 
 def get_song():
